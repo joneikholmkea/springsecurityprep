f2df4e215d6d1661d3c000587262f23540672c71
Jon Eikholm <jone@kea.dk>
2020-10-28 13:33:13 +0100

    first

---
+++ src/main/java/com/eikholm/securityprep/SecurityprepApplication.java
@@ -0,0 +1,17 @@
+package com.eikholm.securityprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// https://www.baeldung.com/java-config-spring-security
+
+
+@SpringBootApplication
+public class SecurityprepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityprepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eikholm/securityprep/SecurityprepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eikholm.securityprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityprepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
